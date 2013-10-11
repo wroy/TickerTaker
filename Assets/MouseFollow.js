843ed68af8f92494f27ce618285c607c03e39b7c
wroy <wroy@uwo.ca>
2013-10-10 20:58:18 -0400

    adding tickertaker to github

---
+++ Assets/MouseFollow.js
@@ -0,0 +1,18 @@
+﻿var smooth = 2.0;
+var tiltAngle = 30.0;
+
+function Start () {
+
+}
+
+function Update () {
+	var halfW : float = Screen.width;
+	transform.position.x = 15 * ((Input.mousePosition.x - (0.5 * halfW)) / halfW);
+	var halfH : float = Screen.height;
+	transform.position.y = 15 * ((Input.mousePosition.y - (0.5 * halfH)) / halfH);
+	
+	var tiltAroundZ = Input.GetAxis("Mouse X") * tiltAngle * -2;
+	var tiltAroundX = Input.GetAxis("Mouse Y") * tiltAngle;
+	var target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+	transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+}
